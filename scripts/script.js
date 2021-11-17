5ad29b206cc9190c9c8194bb37041da2e13951f2
Egor Burenko <step1541@yandex.ru>
2021-11-18 01:09:06 +0300

    feat: доделал вёрстку, добавил JS; style: доделал адаптив

--- scripts/script.js
+++ scripts/script.js
@@ -1,30 +1,32 @@
-const editButton = document.querySelector(".profile__edit-button_link");
-const popup = document.querySelector(".popup");
-const popupClose = popup.querySelector(".popup__close");
+const editButton = document.querySelector('.profile__button_type_edit');
+const popup = document.querySelector('.popup');
+const popupClose = document.querySelector('.popup__close-button');
 
 function open() {
-    popup.classList.add("popup__opened");
+  popup.classList.add('popup_opened');
+
+  nameInput.value = profileName.textContent;
+  jobInput.value = profileJob.textContent;
 }
 
 function close() {
-    popup.classList.remove("popup__opened");
+  popup.classList.remove('popup_opened');
 }
 
-editButton.addEventListener("click", open);
-popupClose.addEventListener("click", close);
+editButton.addEventListener('click', open);
+popupClose.addEventListener('click', close);
 
-let formElement = popup.querySelector(".popup__content")
-let nameInput = popup.querySelector(".popup__text_name");
-let jobInput = popup.querySelector(".popup__text_job");
-
+let formElement = popup.querySelector('.popup__form');
+let nameInput = popup.querySelector('.popup__input_type_name');
+let jobInput = popup.querySelector('.popup__input_type_job');
 
 function formSubmitHandler(evt) {
-    evt.preventDefault();
-    let profileTitle = document.querySelector(".profile__title");
-    let profileText = document.querySelector(".profile__text");
-    profileTitle.textContent = nameInput.value;
-    profileText.textContent = jobInput.value;
-    close();
+  evt.preventDefault();
+  let profileName = document.querySelector('.profile__name');
+  let profileJob = document.querySelector('.profile__job');
+  profileName.textContent = nameInput.value;
+  profileJob.textContent = jobInput.value;
+  close();
 }
 
 formElement.addEventListener('submit', formSubmitHandler);
