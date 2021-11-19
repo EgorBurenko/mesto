198013597824a8bf282e9665ca6979e61bc76e69
Egor Burenko <step1541@yandex.ru>
2021-11-20 02:42:48 +0300

    fix: исправил все замечания ревью; итерация 1

--- scripts/script.js
+++ scripts/script.js
@@ -1,32 +1,33 @@
+// Объявляем переменные, находим элементы в дереве
 const editButton = document.querySelector('.profile__button_type_edit');
 const popup = document.querySelector('.popup');
 const popupClose = document.querySelector('.popup__close-button');
 
-function open() {
-  popup.classList.add('popup_opened');
-
-  nameInput.value = profileName.textContent;
-  jobInput.value = profileJob.textContent;
-}
-
-function close() {
-  popup.classList.remove('popup_opened');
-}
-
-editButton.addEventListener('click', open);
-popupClose.addEventListener('click', close);
-
 let formElement = popup.querySelector('.popup__form');
 let nameInput = popup.querySelector('.popup__input_type_name');
 let jobInput = popup.querySelector('.popup__input_type_job');
+let profileName = document.querySelector('.profile__name');
+let profileJob = document.querySelector('.profile__job');
 
-function formSubmitHandler(evt) {
+// Функция открытия попапа
+function openPopup() {
+  popup.classList.add('popup_opened');
+  nameInput.value = profileName.textContent;
+  jobInput.value = profileJob.textContent;
+}
+// Функция закрытия попапа
+function closePopup() {
+  popup.classList.remove('popup_opened');
+}
+// Функция нажатия на кнопку "Сохранить"
+function handleFormSubmit(evt) {
   evt.preventDefault();
-  let profileName = document.querySelector('.profile__name');
-  let profileJob = document.querySelector('.profile__job');
   profileName.textContent = nameInput.value;
   profileJob.textContent = jobInput.value;
-  close();
+  closePopup();
 }
 
-formElement.addEventListener('submit', formSubmitHandler);
+// Создаём обработчики событий
+editButton.addEventListener('click', openPopup);
+popupClose.addEventListener('click', closePopup);
+formElement.addEventListener('submit', handleFormSubmit);
