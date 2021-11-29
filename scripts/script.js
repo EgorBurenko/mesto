bf4c9bab65630823d9d33dc291e0e78c7eb46b97
Egor Burenko <step1541@yandex.ru>
2021-11-30 01:12:58 +0300

    refactor: Переработал html, template; feat: создаю addPopup

--- scripts/script.js
+++ scripts/script.js
@@ -1,33 +1,69 @@
-// Объявляем переменные, находим элементы в дереве
+//переменные для попапа Профиля: сам попап и кнопки
 const editButton = document.querySelector('.profile__button_type_edit');
-const popup = document.querySelector('.popup');
-const popupClose = document.querySelector('.popup__close-button');
+const profilePopup = document.querySelector('.profile-popup');
+const profilePopupClose = document.querySelector('.profile-popup__close-button');
 
-let formElement = popup.querySelector('.popup__form');
-let nameInput = popup.querySelector('.popup__input_type_name');
-let jobInput = popup.querySelector('.popup__input_type_job');
+//переменные форм ввода и заголовки
+let formElement = profilePopup.querySelector('.profile-popup__form');
+let nameInput = profilePopup.querySelector('.profile-popup__input_type_name');
+let jobInput = profilePopup.querySelector('.profile-popup__input_type_job');
 let profileName = document.querySelector('.profile__name');
 let profileJob = document.querySelector('.profile__job');
 
-// Функция открытия попапа
-function openPopup() {
-  popup.classList.add('popup_opened');
+// Функция открытия попапа Профиля
+function openProfilePopup() {
+  profilePopup.classList.add('profile-popup_opened');
   nameInput.value = profileName.textContent;
   jobInput.value = profileJob.textContent;
 }
-// Функция закрытия попапа
-function closePopup() {
-  popup.classList.remove('popup_opened');
+// Функция закрытия попапа Профиля
+function closeProfilePopup() {
+  profilePopup.classList.remove('profile-popup_opened');
 }
-// Функция нажатия на кнопку "Сохранить"
+// Функция нажатия "Сохранить" в попапе Профиля
 function handleFormSubmit(evt) {
   evt.preventDefault();
   profileName.textContent = nameInput.value;
   profileJob.textContent = jobInput.value;
-  closePopup();
+  closeProfilePopup();
 }
 
-// Создаём обработчики событий
-editButton.addEventListener('click', openPopup);
-popupClose.addEventListener('click', closePopup);
+//обработчики событий для кнопок в разделе о профиле Кусто
+editButton.addEventListener('click', openProfilePopup);
+profilePopupClose.addEventListener('click', closeProfilePopup);
 formElement.addEventListener('submit', handleFormSubmit);
+
+
+//Создание начального блока с карточками по данным из массива
+const initialCards = [
+  {
+    name: 'Архыз',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
+  },
+  {
+    name: 'Челябинская область',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
+  },
+  {
+    name: 'Иваново',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
+  },
+  {
+    name: 'Камчатка',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
+  },
+  {
+    name: 'Холмогорский район',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
+  },
+  {
+    name: 'Байкал',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
+  }
+]; 
+
+//константы для работы с блоком карточек
+const cardsContainerEl = document.querySelector('.elements');
+const templateEl = document.querySelector('.template');
+
+
